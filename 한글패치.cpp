a6140b102ba8e9cdfcca56ea10b0c74a17f89d5a
kogi <whvkek@naver.com>
2020-11-25 16:06:34 +0900

    한글깨짐수정

---
+++ 한글패치.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	//한글 깨짐현상 수정 git bash 명령어 입력
+
+	int n=0;
+	while(true){
+		n++;
+		if(n>10)
+			break;
+	}
+	cout << "테스트" << endl;
+
+	return 0;
+}
